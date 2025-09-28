8658dc89a2561a79c41797db200d8379c37d70ce
Esther <esther.wanderley@uol.com.br>
2025-09-28 19:13:06 -0300

    Tried to have Fun, only signed stuff

--- Solutions/FMCFun.hs
+++ Solutions/FMCFun.hs
@@ -12,13 +12,13 @@ import Prelude hiding
 
 -- curry takes a "traditional" binary function
 -- and returns its currified version
-curry :: undefined
-curry = undefined
+curry :: ((a, b)->c) -> (a->b->c)
+curry f = undefined
 
 -- uncurry takes a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
+uncurry :: (a->b->c) -> ((a, b)->c)
+uncurry f = undefined
 
 -- flip takes a (currified) binary function
 -- and returns one that behaves the same but takes its arguments in the opposite order
@@ -32,8 +32,10 @@ uncurry = undefined
 -- think: why would we ever want that?
 
 -- iterate: figure it out by its type
+-- if this guy         v        was a Num id have an idea...
 iterate :: (a -> a) -> a -> [a]
 iterate = undefined
+-- maybe input/output list...?
 
 -- orbit
 orbit = flip iterate
